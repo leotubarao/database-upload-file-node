d78e552a8f6927d3c02369a337266f9285d784b2
Leônidas Santos <oi@ltco.com.br>
2020-09-06 22:04:32 -0300

    Database relationships; Added CORS

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -27,7 +27,11 @@ class Transaction {
   @Column()
   category_id: string;
 
-  @ManyToOne(() => Category)
+  // @ManyToOne(() => Category)
+  // @JoinColumn({ name: 'category_id' })
+  // category: Category;
+
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
   category: Category;
 
